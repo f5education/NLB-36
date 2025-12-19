241b40e116845c94b89afd4f1837882314f62e21
F5 Education Services <99698691+learnf5@users.noreply.github.com>
2025-12-19 09:42:17 -0500

    Update 20-NLB-36-110-INTRO.sh

--- 20-NLB-36-110-INTRO.sh
+++ 20-NLB-36-110-INTRO.sh
@@ -1,4 +1,4 @@
 # update lab environment
-curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/$GITHUB/$COURSE_ID/main/INTRO/default.conf
+curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/$GHACCT/$COURSE_ID/main/INTRO/default.conf
 sudo scp /tmp/default.conf nginx:/etc/nginx/conf.d/
 sudo ssh nginx sudo rm --force /etc/nginx/conf.d/main.conf
