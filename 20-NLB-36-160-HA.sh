5ecd1ba52d127b91ac4104b77efbb2e91be5067f
F5 Education Services <99698691+learnf5@users.noreply.github.com>
2025-12-18 20:39:23 -0500

    Update 20-NLB-36-160-HA.sh

--- 20-NLB-36-160-HA.sh
+++ 20-NLB-36-160-HA.sh
@@ -1,19 +1,19 @@
 # update lab environment
 sudo ssh nginx test -e /etc/nginx/conf.d/default.conf && sudo ssh nginx rm /etc/nginx/conf.d/default.conf
-curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/$COURSE_ID/main/TCP/main.conf
+curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/$GITHUB/$COURSE_ID/main/TCP/main.conf
 sudo scp /tmp/main.conf nginx:/etc/nginx/conf.d/
-curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/$COURSE_ID/main/HA/nginx.conf
+curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/$GITHUB/$COURSE_ID/main/HA/nginx.conf
 
 #### NEED TO GET NEW UPDATED R34 nginx.conf stuff like mgmt{} context
 sudo scp /tmp/nginx.conf nginx:/etc/nginx/LAB_nginx.conf
 
 # This tcp directory created in lab 5, so it should carry on
 sudo ssh nginx mkdir /etc/nginx/tcp
-curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/$COURSE_ID/main/HA/tcp_lb.conf
+curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/$GITHUB/$COURSE_ID/main/HA/tcp_lb.conf
 sudo scp /tmp/tcp_lb.conf nginx:/etc/nginx/tcp
 
 # Add nginx servers to /etc/hosts file
-curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/$COURSE_ID/main/HA/hosts
+curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/$GITHUB/$COURSE_ID/main/HA/hosts
 sudo scp /tmp/hosts nginx:/etc/hosts
 sudo scp /tmp/hosts nginx2:/etc/hosts
 
